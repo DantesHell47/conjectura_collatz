85a5079a483621be3aec1b59233e27645502fc54
DantesHell47 <Jefferson.dante47@gmail.com>
2023-08-05 13:18:08 -0300

    Adicionado grafico com o numero de iteracoes e os valoes para cada iteracao

--- collatz.jl
+++ collatz.jl
@@ -1,7 +1,8 @@
-using Plots
+using Plots; gr()
 
 function collatz(n)
 	k = 0
+	n_values = []
 	while n > 1
 		if n%2 == 0
 			n = div(n,2)
@@ -9,8 +10,18 @@ function collatz(n)
 			n = 3n + 1
 		end
 		k+=1
+		push!(n_values, n)
 	end
-	return k
+	return k, n_values
 end
 
-	
+plot_collatz = plot(collatz(27)[2], 
+	markershape=:circle, 
+	c=:green, 
+	legend=false, 
+	xlabel="Passos", 
+	ylabel="Valores",
+	xlim=(1, collatz(27)[1]+5))
+vline!([maximum(collatz(27)[1])],ls=:dash, lw=3.,c=:red)
+
+savefig("graph.svg")
