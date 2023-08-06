f5eaadd0fb96048018cd6b36ded515467a87fa6b
DantesHell47 <Jefferson.dante47@gmail.com>
2023-08-06 20:10:30 -0300

    Foi adicioado uma feature para contar a quantidade de numeros pares e impares na funcao collatz

--- collatz.jl
+++ collatz.jl
@@ -2,13 +2,15 @@ using Plots; gr()
 
 function collatz(n)
 	k = 0
+	even = 0
+	odd = 0 
 	n_values = []
 	while n > 1
-		n = n%2 == 0 ? div(n,2) : 3n+1
+		n = n%2 == 0 ? (even+=1; div(n,2)) : (odd+=1; 3n+1)
 		k+=1
 		push!(n_values, n)
 	end
-	return k, n_values
+	return k, n_values, (even, odd)
 end
 
 function search_k(ran, num_k)
@@ -22,16 +24,17 @@ function plots_collatz(values)
 	plot(title="Conjectura de Collatz", xlabel="NUMEROS DE ITERAÇÕES", ylabel="VALORES")
 	for value in values
 		k, lis_values = collatz(value)
+		max = maximum(lis_values)
 		global z = plot!(lis_values, 
 		markershape=:circle,
-		xscale=:log10,
-		yscale=:log10,
-		label="n= $value, k= $k")
+		#xscale=:log10,
+		#yscale=:log10,
+		
+		label="n= $value, k= $k, max=$max",)
 		plot!(legend=:topleft)
 	end
 	return z
 end
 
 
-plots_collatz([77031, 2*77031])
-
+plots_collatz([64, 10])
