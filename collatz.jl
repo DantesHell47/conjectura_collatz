32bd41924f18d00cf65d02963afd0df005daa057
DantesHell47 <Jefferson.dante47@gmail.com>
2023-08-06 21:37:34 -0300

    Melhoras na estilização dos graficos

--- collatz.jl
+++ collatz.jl
@@ -10,7 +10,7 @@ function collatz(n)
 		k+=1
 		push!(n_values, n)
 	end
-	return k, n_values, (even, odd)
+	return k, n_values, even, odd
 end
 
 function search_k(ran, num_k)
@@ -23,18 +23,19 @@ search_k([10,1000], 111)
 function plots_collatz(values)
 	plot(title="Conjectura de Collatz", xlabel="NUMEROS DE ITERAÇÕES", ylabel="VALORES")
 	for value in values
-		k, lis_values = collatz(value)
+		k, lis_values , even, odd = collatz(value)
 		max = maximum(lis_values)
 		global z = plot!(lis_values, 
 		markershape=:circle,
-		#xscale=:log10,
-		#yscale=:log10,
-		
-		label="n= $value, k= $k, max=$max",)
-		plot!(legend=:topleft)
+		xscale=:log10,
+		yscale=:log10,
+		label="n= $value, k= $k, max_value=$max, even=$even, odd=$odd ",)
+		plot!(legend=:bottomleft)
 	end
 	return z
 end
 
 
-plots_collatz([64, 10])
+plots_collatz([27,7031])
+
+savefig("graph.svg")
