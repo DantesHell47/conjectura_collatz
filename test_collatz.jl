b27bdf9bc2b21f4bb7ff6ad770c2bca57bbd804f
DantesHell47 <Jefferson.dante47@gmail.com>
2024-01-24 23:39:25 -0300

    Arquivo de teste para a conjectura de Collatz

--- test_collatz.jl
+++ test_collatz.jl
@@ -1,8 +1,19 @@
 using Test
 include("collatz.jl")
 
+# CONJUNTO DE TESTE PARA DomainError
+@testset "Para n < 0 e não inteiro" begin
+    @test_throws DomainError collatz(0)
+    for i in -100:-1
+        @test_throws DomainError collatz(i)    
+
+    end
+end
+
 # CONJUTO DE TESTE
 @testset "TESTES COM O NUEMERO 27" begin
     @test collatz(27)[1] == 111
     @test collatz(27)[2][end] == 1
+    @test collatz(27)[2][end-1] == 2
+    @test collatz(27)[2][end-2] == 4
 end
