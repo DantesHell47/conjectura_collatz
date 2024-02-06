c5c1cb84e85e48edecee0473885e3746fcfedd0e
DantesHell47 <Jefferson.dante47@gmail.com>
2024-02-05 22:59:55 -0300

    Foi corrigido o erro do código anterior. Nesta versão o código para quando encontra um número repetido na sequencia

--- collatzNegativo.jl
+++ collatzNegativo.jl
@@ -1,14 +1,14 @@
 # Versão para numeros negativos da conjectura de collatz
+#
 
 function collatz_negativa(n)
-    k = 0
-    n_values = []
-    while n != -5
-        n = n%2 == 0 ? div(n,2) : 3n +1 
-        k += 1
-        push!(n_values, n)
+    n_values = Int[]
+    while true
+        n = n%2 == 0 ? n ÷ 2 : 3n + 1
+        if !(n in n_values)
+            push!(n_values, n)
+        else
+            return n_values
+        end
     end
-    return k, n_values
 end
-
-collatz_negativa(-27)
